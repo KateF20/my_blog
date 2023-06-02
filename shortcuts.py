d9bdb5c46fc2f02e078a5d3ab85175dfe57c94e1
Ekaterina Fedoseeva <kaitherinayrd@gmail.com>
2023-06-02 16:39:19 +0200

    setting up for tests

--- shortcuts.py
+++ shortcuts.py
@@ -1,2 +1,13 @@
-def get_object_or_404():
-    pass
+from django.http import Http404
+
+from blog.models import Post
+
+
+def get_post_by_slug(slug):
+    try:
+        post = Post.objects.get(slug=slug)
+
+    except Post.DoesNotExist:
+        raise Http404("No Post matches the given slug.")
+
+    return post
