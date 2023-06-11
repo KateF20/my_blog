742e7798939c0441be5aa549ab087a8dd0b64af7
Ekaterina Fedoseeva <kaitherinayrd@gmail.com>
2023-06-11 16:50:22 +0200

    upload image when creating a post

--- blog/models.py
+++ blog/models.py
@@ -26,7 +26,7 @@ class Post(models.Model):
     author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name='posts')
     title = models.CharField(max_length=100)
     excerpt = models.TextField(max_length=200)
-    image_name = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='posts', null=True)
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
