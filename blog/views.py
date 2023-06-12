02800ceca16e88102b835c95c4852fd75edfeb4a
Ekaterina Fedoseeva <kaitherinayrd@gmail.com>
2023-06-12 20:39:15 +0200

    ListView --> DetailView

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import render
-from django.views.generic import ListView
-from django.views import View
+from django.views.generic import ListView, DetailView
 
 from .models import Post, Author, Tag
 from .forms import CommentForm
@@ -27,35 +26,27 @@ class AllPostsView(ListView):
     context_object_name = 'all_posts'
 
 
-class SinglePostView(View):
-    def get(self, request, slug):
-        post = Post.objects.get(slug=slug)
-        context = {
-            'post': post,
-            'post_tags': post.tags.all(),
-            'comment_form': CommentForm(),
-            'comments': post.comments.all().order_by('-id')
-        }
-        return render(request, 'blog/single_post.html', context)
+class SinglePostView(DetailView):
+    template_name = 'blog/single_post.html'
+    model = Post
+    context_object_name = 'post'
+
+    def get_context_data(self, object, comment_form=None, **kwargs):
+        context = super().get_context_data(object=object, **kwargs)
+        context['comments'] = object.comments.all().order_by('-id')
+        context['post_tags'] = object.tags.all()
+        context['comment_form'] = comment_form or CommentForm()
+        return context
 
     def post(self, request, slug):
+        post = self.object = self.get_object()
         comment_form = CommentForm(request.POST)
-        post = Post.objects.get(slug=slug)
 
         if comment_form.is_valid():
             comment = comment_form.save(commit=False)
             comment.post = post
             comment.save()
-
             return HttpResponseRedirect(reverse('single-post', args=[slug]))
 
-        context = {
-            'post': post,
-            'post_tags': post.tags.all(),
-            'comment_form': comment_form,
-            'comments': post.comments.all().order_by('-id')
-
-        }
-
-        return render(request, 'blog/single_post.html', context)
-
+        context = self.get_context_data(object=self.object, comment_form=comment_form)
+        return self.render_to_response(context)
