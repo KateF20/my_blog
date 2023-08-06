f68dfb9db4aa113699bccaaf06279f40e5de0967
Ekaterina Fedoseeva <kaitherinayrd@gmail.com>
2023-08-06 14:04:18 +0200

    minor changes in views

--- detail_view_func.py
+++ detail_view_func.py
@@ -29,7 +29,6 @@ class DetailView(View):
     def get(self, request, *args, **kwargs):
         self.object = self.get_object()
         context = self.get_context_data(object=self.object)
-        print('get works')
         return self.render_to_response(context)
 
     def get_context_object_name(self):
